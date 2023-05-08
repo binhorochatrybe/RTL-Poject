34a85264dee3e54e84da2a9ce7bcb0c5a8310e8f
BinhodevTrybe <binhuhue@gmail.com>
2023-05-08 00:23:13 -0300

    ../Corrections - trying again

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -3,15 +3,21 @@ import { render, screen } from '@testing-library/react';
 import About from '../pages/About';
 
 describe('Teste o componente <About.js />.', () => {
-  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+  test('Teste se a página contém um heading h2 com o texto About Pokédex;', () => {
     render(<About />);
     const headText = screen.getByRole('heading', { name: /about pokédex/i });
+    expect(headText).toBeInTheDocument();
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex;', () => {
+    render(<About />);
     const info1 = screen.getByText(/this application simulates a pokédex, a digital encyclopedia containing all pokémon/i);
     const info2 = screen.getByText(/one can filter pokémon by type, and see more details for each one of them/i);
-    const image = screen.getByRole('img', { name: /pokédex/i });
-    expect(headText).toBeInTheDocument();
     expect(info1).toBeInTheDocument();
     expect(info2).toBeInTheDocument();
+  });
+  test('Teste se a página contém dois parágrafos com texto sobre a Pokédex;', () => {
+    render(<About />);
+    const image = screen.getByRole('img', { src: 'https://cdn2.bulbagarden.net/upload/thumb/8/86/Gen_I_Pok%C3%A9dex.png/800px-Gen_I_Pok%C3%A9dex.png' });
     expect(image).toBeInTheDocument();
   });
 });
--- src/tests/NotFound.test.js
+++ src/tests/NotFound.test.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import NotFound from '../pages/NotFound';
 
-describe('Teste o componente <About.js />.', () => {
-  test('Teste se a página contém as informações sobre a Pokédex;', () => {
+describe('Teste o componente <NotFound.js />', () => {
+  test('Teste se a página contém um heading h2 com o texto Page requested not found;', () => {
     render(<NotFound />);
     const heading = screen.getByRole('heading', { name: /page requested not found/i });
+    expect(heading).toBeInTheDocument();
+  });
+  test('Teste se a página mostra a imagem https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif.', () => {
+    render(<NotFound />);
     const image = screen.getByRole('img', { src: 'https://media.giphy.com/media/kNSeTs31XBZ3G/giphy.gif' });
     expect(image).toBeInTheDocument();
-    expect(heading).toBeInTheDocument();
   });
 });
